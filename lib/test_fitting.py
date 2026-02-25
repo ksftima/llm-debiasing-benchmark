37b2b99a93d2cf8302d589523cbbbf0b2e46b742
kesa <kesafatimaa@gmail.com>
2026-02-25 15:07:35 +0100

    Fix test_fitting to not import from vary_expert_simulation

--- lib/test_fitting.py
+++ lib/test_fitting.py
@@ -1,6 +1,28 @@
 import numpy as np
+import scipy
 from fitting import logit_fit, logit_fit_ppi
-from vary_expert_simulation import generate
+
+
+def generate(num_samples, prediction_accuracy):
+    M = 10
+    means = np.zeros(M)
+    covs = 0.3 * np.ones((M, M))
+    covs = covs + 0.7 * np.eye(M)
+    X = np.random.multivariate_normal(means, covs, size=num_samples)
+    X[:,1] = (X[:,1] > scipy.stats.norm.ppf(0.8)).astype(np.float32)
+    X1, X2, X3, X4, X6 = X[:,0], X[:,1], X[:,2], X[:,3], X[:,5]
+    a = 0.1 / (1.0 + np.exp(0.5 * X3 - 0.5 * X2))
+    b = 1.3 * X4 / (1.0 + np.exp(-0.1 * X2))
+    c = 1.5 * X4 * X6
+    d = 0.5 * X1 * X2
+    e = 1.3 * X1
+    f = X2
+    W = a + b + c + d + e + f
+    Y_true = np.random.binomial(1, scipy.special.expit(W), size=num_samples).astype(float)
+    X_train = np.stack([X[:,0], X[:,0] ** 2, X[:,1], X[:,3]], axis=1)
+    p = np.random.binomial(1, prediction_accuracy, size=num_samples)
+    Y_pred = p * Y_true + (1 - p) * (1 - Y_true)
+    return X_train, Y_true, Y_pred
 
 np.random.seed(42)
 
